810a5bd4ca1fd0d5b2c8806907ac4d2794bca079
brenoaugustodev <brenoaugustocorretor@gmail.com>
2023-09-24 12:04:28 -0300

    subindo aplicacao

---
+++ Forensic_Bones/Forensic_Bones/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Forensic_Bones.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
