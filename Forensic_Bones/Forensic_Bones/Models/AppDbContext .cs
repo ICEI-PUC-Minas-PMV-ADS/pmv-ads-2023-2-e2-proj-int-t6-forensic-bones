ade4165ab290b41b5b991feb2b86f1d54dee09c0
ljbrandao <luilson.brandao@gmail.com>
2023-12-02 22:28:09 -0300

    Correção commit

---
+++ Forensic_Bones/Forensic_Bones/Models/AppDbContext .cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Forensic_Bones.Models;
+
+namespace Forensic_Bones.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Relatorio> Relatorios { get; set; }
+        public DbSet<InventarioEsqueleto> InventariosEsqueleto { get; set; }
+        public DbSet<InventarioCranio> InventarioCranio { get; set; }
+        public DbSet<MarcadoresCranio> MarcadoresCranio { get; set; }
+
+        // Configurações adicionais do modelo, se necessário, podem ser feitas aqui
+       
+    }
+}
---
+++ Forensic_Bones/Forensic_Bones/Models/MarcadoresCranio.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Forensic_Bones.Models
+{
+    [Table("MarcadoresCranio")]
+    public class MarcadoresCranio
+    {
+
+        [Key]
+        public int MarcadoresCranioId { get; set; }
+
+        [Required]
+        [Display(Name = "Código do Relatório")]
+        public int RelatorioId { get; set; }
+
+        [Display(Name = "Crista Nucal")]
+        public char CristaNucal { get; set; }
+
+        [Display(Name = "Processo Mastoide")]
+        public char ProcessoMastoide { get; set; }
+
+        [Display(Name = "Eminência Mentoniana")]
+        public char EminenciaMentoniana { get; set; }
+
+        [Display(Name = "Supra Orbital")]
+        public char SupraOrbital { get; set; }
+
+        [Display(Name = "Área da Glabela")]
+        public char AreaGlabela { get; set; }
+
+        [Display(Name = "Observações")]
+        public string Observacoes { get; set; }
+
+        [ForeignKey("RelatorioId")]
+        public virtual Relatorio Relatorio { get; set; }
+        public InventarioCranio InventarioCranio { get; set; }
+    }
+}
